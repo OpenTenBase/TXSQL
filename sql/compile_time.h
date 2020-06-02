d3488ca19b4d7507fb0e7d3ac97dea972cf66688
harlylei <harlylei@tencent.com>
2020-06-02 19:25:22 +0800

    Issue #154 CompileTime function return not correct month
    fix compute month's algorithm.
    distinguish jan and jun

--- sql/compile_time.h
+++ sql/compile_time.h
@@ -4,11 +4,10 @@
 #define CUR_YEAR ((((__DATE__ [7] - '0') * 10 + (__DATE__ [8] - '0')) * 10 \
     + (__DATE__ [9] - '0')) * 10 + (__DATE__ [10] - '0'))
 
-#define CUR_MONTH ((__DATE__ [2] == 'n' ? 0 \
+#define CUR_MONTH ((__DATE__ [2] == 'n' ?  (__DATE__ [1] == 'a' ? 0 : 5) \
     : __DATE__ [2] == 'b' ? 1 \
     : __DATE__ [2] == 'r' ? (__DATE__ [0] == 'M' ? 2 : 3) \
     : __DATE__ [2] == 'y' ? 4 \
-    : __DATE__ [2] == 'n' ? 5 \
     : __DATE__ [2] == 'l' ? 6 \
     : __DATE__ [2] == 'g' ? 7 \
     : __DATE__ [2] == 'p' ? 8 \
@@ -28,6 +27,3 @@ const char *CompileTime () {
     snprintf (tmpbuf , sizeof(tmpbuf) , "-v18-txsql-1.0.0-V2.0R613D001-%04d%02d%02d-%02d%02d" , CUR_YEAR , CUR_MONTH , CUR_DAY , CUR_HOUR , CUR_MIN);
     return tmpbuf;
 }
-
-
-//const char *CompileTime="-V1.0R013D004-20190710-1030";
