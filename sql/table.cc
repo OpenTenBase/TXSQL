ea1fa8b798deceefc918b7a8a8246e5e5b3ae4b7
lucaszhai <lucaszhai@tencent.com>
2021-12-22 10:18:01 +0800

    Issue #470 ASAN reports ERROR IN TABLE::is_perfix_index
    In function TABLE::is_perfix_index, the paramater key maybe -1 but
    get used as array iterater which leads to heap-buffer-overflow error
    reported by ASAN.
    
    Fix: return from function directly if key = -1

--- sql/table.cc
+++ sql/table.cc
@@ -7856,7 +7856,7 @@ void TABLE::update_covering_prefix_keys(Field *field, uint16 key_read_length,
 
 bool TABLE::is_perfix_index(int key, uint key_parts) {
 
-  if (!this->key_info) {
+  if (!this->key_info || key == -1) {
     return false;
   }
   KEY_PART_INFO *key_part = this->key_info[key].key_part;
