46acae8897d32f0237f011ab5583372abacf97e2
hcduan <hcduan@tencent.com>
2022-08-11 16:00:59 +0800

    [bugfix] #1296 Support EXPLAIN fallback
    Problem:
    =======
      Optimizer context caches info when executing or explaining an SQL 
      statement. The memory limit of the optimizer context
      (txsql_optimizer_context_max_mem_size) could cause EXPLAIN to
      complain instead of printing the plan.
    
    Solution:
    ========
      When the optimizer context memory limit is reached, fallback the
      EXPLAIN to serial mode as real execution will do.
    
    MR!1205 reviewed by kenkkchen, maxmqiu

--- sql/parallel_execution/px_optimizer_context.cc
+++ sql/parallel_execution/px_optimizer_context.cc
@@ -421,6 +421,7 @@ void Opt_ctx_client::set_ctx(enum enum_opt_ctx_mode mode,
     }
 #endif
   } else {
+    m_mode = OPT_CTX_NATIVE;
 #ifndef DBUG_OFF
     m_dbug_session = &m_thd->opt_dbug_session;
 #endif
@@ -813,6 +814,13 @@ class Px_optimizer_context_error_handler : public Internal_error_handler {
             (ulonglong)thd->variables.txsql_optimizer_context_max_mem_size,
             "ER_PX_CAPACITY_EXCEEDED_IN_OPTIMIZER_CONTEXT",
             ER_THD(thd, ER_PX_CAPACITY_EXCEEDED_IN_OPTIMIZER_CONTEXT));
+
+        if (thd->lex->is_explain()) {
+          /* close the cache and fallback to serial explain */
+          OPT_CTX(thd).set_ctx(OPT_CTX_NATIVE);
+          return true;
+        }
+
 #if defined(HAVE_PX)
         mysql_mutex_lock(&LOCK_inc_txsql_parallel_stmt_memory_refused);
         txsql_parallel_stmt_memory_refused++;
