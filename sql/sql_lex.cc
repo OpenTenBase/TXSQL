e19e3a89075344ddb752d27f7bf493252968212b
hcduan <hcduan@tencent.com>
2022-09-01 11:28:37 +0800

    [bugfix] #1377 postfix of consistent view of global vars during query execution
    Problem:
    =======
      The global vars are singleton, which means that they can be modified anytime,
      by any thread. In parallel execution, different status of vars determine
      different branches of code. Thus, in order to ensure consistency, we need to
      keep the global vars unchanged during the execution of a query.
    
    Solution:
    ========
      Keep the global vars unchanged whether coordinator or worker. The worker
      directly use the shadow copy of global vars cached by the coordinator.
    
    MR!1277 reviewed by kenkkchen, maxmqiu

--- sql/sql_lex.cc
+++ sql/sql_lex.cc
@@ -868,13 +868,15 @@ bool LEX::check_px_execution() const {
   // By definition, zero txsql_parallel_degree disables parallel execution
   // except that has been specified in hint. It should be noted that the
   // specified or default value cannot exceed the global resource limit.
-  if (get_parallel_degree_hint(thd, false) != UINT_MAX32) {
-    if(get_parallel_degree_hint(thd, false) > txsql_max_parallel_worker_threads) {
+  if(PX_ROLE_USER(thd)){
+    if (get_parallel_degree_hint(thd, false) != UINT_MAX32) {
+      if(get_parallel_degree_hint(thd, false) > PX_MAX_THREADS(thd)) {
+        return false;
+      }
+    } else if (!thd->variables.txsql_parallel_degree ||
+        thd->variables.txsql_parallel_degree > PX_MAX_THREADS(thd)) {
       return false;
     }
-  } else if (!thd->variables.txsql_parallel_degree ||
-      thd->variables.txsql_parallel_degree > txsql_max_parallel_worker_threads) {
-    return false;
   }
 
   if (thd->is_attachable_transaction_active() ||
