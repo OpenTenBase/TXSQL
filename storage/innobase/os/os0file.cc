937d6dd7fe6822e281039f50a5e69449d21c7e3f
lucasjhlu <lucasjhlu@tencent.com>
2022-10-09 16:30:31 +0800

    [bugfix] followup issue#152 TK2 startup is slow Problem: ======== os_file_truncate has a different return value than ftruncate, emitting useless error messages. Solution: ======== Fix the IF statement.

--- storage/innobase/os/os0file.cc
+++ storage/innobase/os/os0file.cc
@@ -5535,12 +5535,12 @@ bool os_file_set_size_fast(const char *name, pfs_os_file_t pfs_file,
 
 bool os_file_set_size_txsql(const char *name, pfs_os_file_t file,
                             os_offset_t offset, os_offset_t size, bool flush) {
-  if (os_file_truncate(name, file, 0) != 0) {
+  if (os_file_truncate(name, file, 0) == false) {
     ib::info(ER_IB_MSG_1359) << "ftruncate() failed with errno " << errno
                              << " - falling back to writing NULLs.";
     return os_file_set_size(name, file, offset, size, flush);
   }
-  if (os_file_truncate(name, file, size) != 0) {
+  if (os_file_truncate(name, file, size) == false) {
     ib::info(ER_IB_MSG_1359) << "ftruncate() failed with errno " << errno
                              << " - falling back to writing NULLs.";
     return os_file_set_size(name, file, offset, size, flush);
