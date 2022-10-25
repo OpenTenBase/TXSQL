7e0d6a245e7ed204da98bd739ee5d4fc7cfd0e4a
casazhang <casazhang@tencent.com>
2022-10-25 15:37:29 +0800

    [bugfix] #203 fix range estimation by histogram bug.

--- sql/handler.cc
+++ sql/handler.cc
@@ -6347,8 +6347,7 @@ ha_rows handler::multi_range_read_info_const(uint keyno, RANGE_SEQ_IF *seq,
           if (HA_POS_ERROR ==
               (rows = this->records_in_range(keyno, min_endp, max_endp))) {
             /* Can't scan one range => can't do MRR scan at all */
-            total_rows = HA_POS_ERROR;
-            break;
+            return HA_POS_ERROR;
           }
           total_rows += rows;
           continue;
@@ -6368,8 +6367,7 @@ ha_rows handler::multi_range_read_info_const(uint keyno, RANGE_SEQ_IF *seq,
           if (HA_POS_ERROR ==
               (rows = records_in_range(keyno, min_endp, max_endp))) {
             /* Can't scan one range => can't do MRR scan at all */
-            total_rows = HA_POS_ERROR;
-            break;
+            return HA_POS_ERROR;
           }
         } else {
           trustable_zero_estimate = false;
