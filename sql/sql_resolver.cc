320095339c9f54280e60f43506c9538a13186812
qiaohongyu <q.h.rain@163.com>
2026-06-05 11:19:57 +0800

    [port] issue#1002 Bug#35410465: Subquery ORDER BY is ignored
    This is a regression from Bug#34890862. That fix
    made a change for already resolved ORDER BY items
    to not be re-resolved (it is usually the case when
    a derived table is merged). As part of the change,
    when comparing the order by items to the visible
    fields in the outer query block, it compared the
    order by item with the referenced item in the
    ref_item_array. For the case in bugpage, the order
    by item itself is a reference. This makes the
    comparison fail.
    So we now compare the real_item() of the order by
    expression to the real_item() of the ref_item_array
    elements.
    Also, when a hidden item is added, the original fix
    missed to set order->item to point to the correct
    ref array item like what is done in
    find_order_in_list().
    
    Change-Id: I00786757ef64df7390ad20c971baa7086d214e18
    
    Original-Author: Chaithra Gopalareddy <chaithra.gopalareddy@oracle.com>
    Original-Date: Tue May 23 18:35:49 2023 +0530
    Original-Commit: 85abd6e43a1855ceaea32d93a5cbdca7df8e8ca7

--- sql/sql_resolver.cc
+++ sql/sql_resolver.cc
@@ -4535,11 +4535,12 @@ bool setup_order(THD *thd, Ref_item_array ref_item_array, TABLE_LIST *tables,
       // Update with the correct ref item.
       uint counter = fields->size();
       for (uint i = 0; i < fields->size(); i++) {
-        if (order_item->eq(ref_item_array[i]->real_item(), false)) {
+        if (order_item->real_item()->eq(ref_item_array[i]->real_item(),
+                                        false)) {
           order->item = &ref_item_array[i];
           // Order by is now referencing select expression, so increment the
           // reference count for the select expression.
-          (*order->item)->increment_ref_count();
+          // (*order->item)->increment_ref_count();
           order->in_field_list = true;
           counter = i;
           break;
@@ -4551,6 +4552,7 @@ bool setup_order(THD *thd, Ref_item_array ref_item_array, TABLE_LIST *tables,
         fields->push_front(order_item);
         order_item->hidden = true;
         order->in_field_list = false;
+        order->item = &ref_item_array[counter];
       }
       continue;
     }
